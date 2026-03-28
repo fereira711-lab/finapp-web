5a9198f38da315213d747e0ac500b1b3bee8d456
Rafael Ferreira <fereira711@gmail.com>
2026-03-27 22:08:49 -0300

    fix: header e bottom nav nao tampam conteudo

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -9,7 +9,7 @@ export default function AppShell({ children }: { children: React.ReactNode }) {
     <div className="min-h-screen">
       <Sidebar />
       <MobileHeader />
-      <main className="md:ml-64 pt-16 pb-24 md:py-8 min-h-screen px-4 md:px-10 md:flex md:items-center md:justify-center">
+      <main className="md:ml-64 pt-16 pb-20 md:py-8 min-h-screen px-4 md:px-10 md:flex md:items-center md:justify-center">
         <div className="glass w-full max-w-3xl p-4 md:p-6 mx-auto">
           {children}
         </div>
